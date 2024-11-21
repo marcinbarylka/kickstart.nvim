650d1a1bad69efc1f238fe9163b73ece0fecb2c2
Marcin Baryłka <marcin.barylka@gmail.com>
2024-11-21 19:14:16 +0100

    new color schemes

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,16 +1,8 @@
 -- NOTE
 -- Here go custom options for my setup:
 
--- require('gruvbox').setup {
---   italic = {
---     strings = false,
---     emphasis = false,
---     comments = false,
---   },
--- }
-
 vim.g.python3_host_prog = 'home/kaczor/.pyenv/versions/nvim/bin/python'
-vim.cmd.colorscheme 'vscode'
+vim.cmd.colorscheme 'bamboo'
 vim.wo.colorcolumn = '120' -- highlight the 120th column
 
 vim.o.showtabline = 2
--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -1,4 +1,12 @@
 return {
   { 'Mofiqul/vscode.nvim' },
-  { 'olimorris/onedarkpro.nvim' }
+  { 'olimorris/onedarkpro.nvim' },
+  { 'rebelot/kanagawa.nvim' },
+  { 'ribru17/bamboo.nvim' },
+  {
+    "HoNamDuong/hybrid.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {}
+  },
 }
